071ab77ba267f79fa91c498101b69e9fe8de0624
Nikolai Golub <nikolai.golub@consensys.net>
2022-08-08 18:13:37 +0200

    Use same scalar for comparison

--- benches/eth_pairings.rs
+++ benches/eth_pairings.rs
@@ -5,19 +5,19 @@ use eth_pairings::public_interface::decode_g1::serialize_g1_point;
 use eth_pairings::public_interface::eip196::EIP196Executor;
 use eth_pairings::weierstrass::Group;
 
+use num_bigint::BigUint;
+
 fn mul_eth_pairings(c: &mut Criterion) {
-    // let p_x = vec![212, 90, 205, 88, 47, 240, 131, 117, 58, 234, 55, 169, 156, 27, 91, 70, 205, 209, 135];
-    // let p_y = vec![100, 20, 30];
-    // let p_x = Fp::from_be_bytes(&*BN254_BASE_FIELD, &p_x, true).unwrap();
-    // let p_y = Fp::from_be_bytes(&*BN254_BASE_FIELD, &p_y, true).unwrap();
-    // let point = CurvePoint::point_from_xy(&*BN254_G1_CURVE, p_x, p_y);
     let g1 = &*BN254_G1_GENERATOR;
     println!("eth_pairings g1: {:?}", g1.into_xy());
     let mut serialized = serialize_g1_point(32, g1).unwrap();
-    let worst_case_scalar = [u64::MAX; 4];
-    println!("WORST SCALAR: {:?}", &worst_case_scalar);
+    let max_element = "21888242871839275222246405745257275088548364400416034343698204186575808495616".parse::<BigUint>().unwrap();
+    let worst_case_scalar = max_element.to_u64_digits();
 
-    for i in worst_case_scalar {
+    // let worst_case_scalar = [u64::MAX; 4];
+    // println!("WORST SCALAR: {:?}", &worst_case_scalar);
+
+    for i in &worst_case_scalar {
         let mut be = i.to_be_bytes().to_vec();
         serialized.append(&mut be);
     }
--- benches/pairing_ce.rs
+++ benches/pairing_ce.rs
@@ -1,23 +1,15 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 use pairing_ce::bn256::*;
-use pairing_ce::{CurveProjective};
-use rand::{Rand, SeedableRng, XorShiftRng};
+use pairing_ce::{CurveAffine, CurveProjective, from_hex};
 
 fn mul_pairing_ce(c: &mut Criterion) {
-    let g1 = G1::one();
+    let g1 = G1::one().into_affine();
     println!("pairing_ce G1: {:?}", g1);
-    let mut rng = XorShiftRng::from_seed([0x5dbe6259, 0x8d313d76, 0x3237db17, 0xe5bc0654]);
-    let fr = Fr::rand(&mut rng);
-    println!("FR: {:?}", fr);
-    // "0x0000000000000000000000000000000000000000000000000000000000000001"
-    // "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
-    // let fr: _ = from_hex::<Fr>("0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff").unwrap();
-    // g1.mul_assign(fr);
-    // println!("G1: {:?}", g1);
+    let fr: Fr = from_hex::<Fr>("0x30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000000").unwrap();
     c.bench_function("pairing_ce.mul_assign", |b| b.iter(|| {
-        let mut local_g1 = g1.clone();
-        local_g1.mul_assign(black_box(fr));
+        let res = g1.mul(black_box(fr));
+        assert!(!res.is_zero())
     }));
 }
 
